dc1cca7bd8324a0727b5fe315b8932fb323636a5
phamin2001 <amin.pahlavani@gmail.com>
2018-12-12 14:53:48 -0700

    Commit 9 - Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -142,3 +142,13 @@ for (let i = 0; i < kristynsCloset.length ; i++) {
 for(let i = 0; i < thomsCloset.length; i++) {
     console.log(thomsCloset[i]);
 }
+
+
+// Multiples of 3 and 5
+let sum = 0;
+for(let i = 1; i < 1000; i++) {
+    if(i % 3 === 0 || i % 5 === 0) {
+        sum = sum + i;
+    }
+}
+console.log("Sum of all the multiples of 3 or 5 below 1000 is: " + sum);
